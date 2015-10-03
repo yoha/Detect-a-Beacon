b938a06409d0a51d93baf0cdbb651b68eaae4fc6
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-10-03 15:30:26 +0700

    Setup beacon's scanning & detection logic.

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -25,7 +25,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         guard status == .AuthorizedWhenInUse else { return }
         guard CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion.self) else { return }
         guard CLLocationManager.isRangingAvailable() else { return }
-        // do stuff
+        self.startScanning()
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        guard beacons.count > 0 else {
+            self.updateDistance(.Unknown)
+            return
+        }
+        let beacon = beacons[0]
+        self.updateDistance(beacon.proximity)
     }
     
     // MARK: - Methods Override
@@ -45,6 +54,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Local Methods
+    
+    func startScanning() {
+        let uuid = NSUUID(UUIDString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "myBeacon")
+        
+        self.locationManager.startMonitoringForRegion(beaconRegion)
+        self.locationManager.startRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    func updateDistance(distance: CLProximity) {
+        UIView.animateWithDuration(0.8) { [unowned self] () -> Void in
+            switch distance {
+            case .Unknown:
+                self.view.backgroundColor = UIColor.grayColor()
+                self.distanceReadingLabel.text = "UNKNOWN"
+                
+            case .Far:
+                self.view.backgroundColor = UIColor.greenColor()
+                self.distanceReadingLabel.text = "FAR"
+                
+            case .Near:
+                self.view.backgroundColor = UIColor.orangeColor()
+                self.distanceReadingLabel.text = "NEAR"
+                
+            case .Immediate:
+                self.view.backgroundColor = UIColor.redColor()
+                self.distanceReadingLabel.text = "RIGHT HERE"
+            }
+        }
+    }
 }
 
